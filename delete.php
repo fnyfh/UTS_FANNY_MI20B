f041bc90cd28df98d9e22a8966c2adc715397b3d
fnyfh <90744361+fnyfh@users.noreply.github.com>
2021-11-03 12:45:31 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -5,7 +5,9 @@ $con = new mysqli("localhost", "root", "", "anggota_fanny");
 
 if (isset($_POST['delete'])) {
   $id = $_POST['id'];
+  // var_dump($id);
+  // die;
 
-  $result = mysqli_query($con, "DELETE FROM `tbl_anggota` WHERE `tbl_anggota`.`id` = $id");
+  $result = mysqli_query($con, "DELETE FROM `tbl_anggota` WHERE id = '$id'");
   header("Location:view.php?pesan=success&&frm=del");
 }
